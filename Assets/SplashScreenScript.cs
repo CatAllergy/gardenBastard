b76529744a9d74871d0048c25b6327f83825ca32
Simon Carlile <simoncarlile@gmail.com>
2018-01-28 12:18:23 +0000

    Initial commit

---
+++ Assets/SplashScreenScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     Invoke("LoadNextScene", 1f);
+
+    }
+    void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        print(currentSceneIndex);
+        int nextSceneIndex = currentSceneIndex + 1; // todo do the right the thing
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);// todo allow for more than 2 levels
+    }
+}
