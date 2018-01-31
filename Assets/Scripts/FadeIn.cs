9409df2cd7c6116895c3a4fff81610f2fab2848e
Simon Carlile <simoncarlile@gmail.com>
2018-01-31 00:05:18 +0000

    Up to lecture 137

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public float fadeSpeed = 2f;
+    private Image image;
+
+	// Use this for initialization
+	void Start () {
+        image = gameObject.GetComponentInChildren<Image>();
+        image.canvasRenderer.SetAlpha(0f);
+        fadeInImage();
+        Invoke("fadeOutImage",4f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void fadeInImage()
+    {
+        //float imageAlpha = image.color.a;
+        
+        image.CrossFadeAlpha(1f,1.5f, false);
+    }
+    void fadeOutImage()
+    {
+        image.CrossFadeAlpha(0f, 1.5f, false);
+    }
+}
