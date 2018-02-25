3d41d8a2b03417a940ebaea2013a8f966d9d5c31
Simon Carlile <simoncarlile@gmail.com>
2018-02-25 23:10:21 +0000

    enemies kill friends

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,26 +7,42 @@ public class Attacker : MonoBehaviour {
     [Range(-1f, 1.5f)]
     private float currentSpeed;
     private GameObject currentTarget;
+    private Animator animationControl;
 
 	// Use this for initialization
 	void Start () {
         //Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
         //myRigidBody.isKinematic = true;
+        animationControl = gameObject.GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
-        print(currentTarget);
-	}
+        //print(currentTarget);
+        if (!currentTarget)
+        {
+            animationControl.SetBool("isAttacking", false);
+        }
+    }
     public void SetSpeed (float speed)
     {
         currentSpeed = speed;
     }
+
+    // Called from the animator at the time of the actual blow
     void StrikeCurrentTarget(float damage)
     {
-        //Debug.Log(name + " dealt" + damage + " damage");
-
+        if (currentTarget)
+        {
+            //Debug.Log(name + " dealt" + damage + " damage");
+            health hp = currentTarget.GetComponent<health>();
+            if (hp)
+            {
+                hp.DealDamage(damage);
+            }
+        }
     }
     public void Attack (GameObject obj)
     {
---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour {
+
+    public float hitpoints = 500f;
+
+    public void DealDamage (float damage)
+    {
+        hitpoints -= damage;
+        if(hitpoints <= 0)
+        {
+            DestroyDefender();
+        }
+    }
+    public void DestroyDefender()
+    {
+        Destroy(gameObject);
+    }
+}
