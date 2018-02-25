5747a1b0462ecfecfdbfebb07b5fc7e6ca461915
Simon Carlile <simoncarlile@gmail.com>
2018-02-25 22:37:59 +0000

    blimey

---
+++ Assets/Scripts/fox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Attacker))]
+
+public class fox : MonoBehaviour
+{
+    
+    private Animator foxAnimator;
+    private Attacker attacker;
+    // Use this for initialization
+    void Start()
+    {
+        foxAnimator = gameObject.GetComponent<Animator>();
+        attacker = GetComponent<Attacker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        GameObject obj = collision.gameObject;
+        //print(obj);
+        if (!obj.GetComponent<Defenders>())
+        {
+            return;
+        }
+        if (obj.GetComponent<Stones>())
+        {
+            foxAnimator.SetTrigger("jump trigger");
+        }
+        else
+        {
+            foxAnimator.SetBool("isAttacking", true);
+            attacker.Attack(obj);
+        }
+        //Debug.Log("Fox hit " + collision);
+    }
+}
---
+++ Assets/lizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof (Attacker))]
+
+public class lizard : MonoBehaviour
+{
+    
+    private Animator lizAnimator;
+    private Attacker attacker;
+    // Use this for initialization
+    void Start()
+    {
+        lizAnimator = gameObject.GetComponent<Animator>();
+        attacker = GetComponent<Attacker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        GameObject obj = collision.gameObject;
+        //print(obj);
+        if (!obj.GetComponent<Defenders>())
+        {
+            return;
+        }
+        
+        lizAnimator.SetBool("isAttacking", true);
+        attacker.Attack(obj);
+     
+        //Debug.Log("Fox hit " + collision);
+    }
+}
